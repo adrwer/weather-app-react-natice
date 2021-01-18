249c6f509ee7da5b2b1026a1d38e07d43d15b011
Adrian Werimo <adrwer7@gmail.com>
2021-01-18 13:39:57 +0300

    Get Location

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+const WEATHER_API_KEY = ''
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  
+  useEffect(() => {
+    load()
+  }, [])
+  
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app')
+        return
+      } 
+
+      const location = await Location.getCurrentPositionAsync()
+
+      const {latitude, longitude} = location.coords
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`)
+    } catch (error) {}
+
+  }  
+
   return (
     <View style={styles.container}>
       <Text>Hello from react native!</Text>
@@ -14,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fbbbff',
     alignItems: 'center',
     justifyContent: 'center',
   },
